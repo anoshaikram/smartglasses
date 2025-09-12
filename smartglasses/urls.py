3d10714aa1facec5292c33748ac36decbc90efd7
Anosha Ikram <ikramanosha04@gmail.com>
2025-09-13 02:17:04 +0500

    Fix home view import in urls

--- smartglasses/urls.py
+++ smartglasses/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core import views
+
 
 urlpatterns = [
-    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),   # include all app routes
 ]
